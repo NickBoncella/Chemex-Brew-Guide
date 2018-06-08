a6562c0fb3d66bbcca649ba2b51181c3d1be6af5
Nick Boncella <njb371@protonmail.com>
2018-06-08 05:44:02 -0400

    Create modal, add modal content and event listeners

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 // === ADD EVENT LISTNER TO BUTTON ===
 
-const calcBtn = document.querySelector('.calc-btn');
-calcBtn.addEventListener('click', function() {
-  
-  console.log('clicked');
-})
+const modalBtn = document.getElementById('modal-btn');
+const modal = document.getElementById('modal');
+const closeBtn = document.querySelector('.close');
+const calcCoffee = document.querySelector('.calc-coffee');
+
+// open modal on click
+modalBtn.addEventListener('click', function() {
+  modal.style.display = 'block';
+});
+
+// close modal on click
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+// calculate water/coffee ratio and close modal
+calcCoffee.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
